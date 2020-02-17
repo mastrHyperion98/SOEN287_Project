64786ad0b38d5c0054ca3e1d4afaeeeb3c5aeb8b
Steven <steven.smith1998@hotmail.com>
2020-02-17 03:57:10 -0500

    Add a couple routes

--- Assignment2/app.py
+++ Assignment2/app.py
@@ -7,6 +7,13 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/dashboard')
+def dashboard():
+    return 'DASHBOARD'
+
+@app.route('/usr/settings-<string:user>')
+def user_settings(user):
+    return user
 
 if __name__ == '__main__':
     app.run()
