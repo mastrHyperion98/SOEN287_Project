448b9220bb830069ac29217954b894538bbf0cd5
Steven <steven.smith1998@hotmail.com>
2020-02-28 22:26:05 -0500

    Create Account Settings

--- A2/app.py
+++ A2/app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, url_for, redirect, request, session
+import os
+
+from flask import Flask, render_template, url_for, redirect, request, session, current_app, send_from_directory, \
+    send_file
 
 app = Flask(__name__)
 
@@ -18,6 +21,11 @@ def create_account():
     return render_template("CreateAccount.html")
 
 
+@app.route('/createAccount/verify', methods=['POST'])
+def verify_createAccount():
+    return redirect(url_for("login"))
+
+
 @app.route('/login/verify', methods=['POST'])
 def verify_login():
     #here we can verify the login credential before redirecting to the dashboard
@@ -32,12 +40,18 @@ def dashboard():
 
 @app.route('/settings/account')
 def account_settings():
-    return render_template("AccountSettings.html")
+    return render_template("AccountSettings.html", username="Hyperion", email="Mastr.hyperion98@gmail.com", permalink="128UA90NV67M")
 
 
 @app.route('/channels')
 def channels():
     return render_template("Channels.html")
+
+
+@app.route('/download/<string:permalink>')
+def download_user_data(permalink):
+	path = "Data/" +permalink + ".txt"
+	return send_file(path, as_attachment=True)
 
 
 if __name__ == '__main__':
