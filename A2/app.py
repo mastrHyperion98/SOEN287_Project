87850a720f781f77676bec3b8365fac207df9bf4
mastrHyperion98 <s_smith1998@outlook.com>
2020-04-11 01:09:16 -0400

    Update to validations and Create Account Complete

--- A2/app.py
+++ A2/app.py
@@ -2,7 +2,7 @@ import os
 import json
 from flask import Flask, render_template, url_for, redirect, request, session, current_app, send_from_directory, \
     send_file
-from forms import LoginForm
+from forms import LoginForm, CreateAccount
 
 app = Flask(__name__)
 app.secret_key = 'allo'
@@ -22,10 +22,11 @@ def login():
 
 @app.route('/create-account', methods=['POST', 'GET'])
 def create_account():
-    form = LoginForm()
+    form = CreateAccount()
     if form.validate_on_submit():
         return redirect(url_for("login"))
-    return render_template("CreateAccount.html")
+    return render_template("CreateAccount.html", form=form)
+
 
 @app.route('/dashboard')
 def dashboard():
--- A2/forms.py
+++ A2/forms.py
@@ -1,10 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextField, SubmitField, PasswordField, SelectField
 from wtforms.fields.html5 import EmailField, DateField, IntegerField
-from wtforms.validators import InputRequired, Email, Length, Regexp, ValidationError, NumberRange
+from wtforms.validators import InputRequired, Email, Length, Regexp, ValidationError, NumberRange, EqualTo, DataRequired
 
 
 class LoginForm(FlaskForm):
-    email = EmailField('Email', validators=[InputRequired(), Email()])
-    password = PasswordField('Password', validators=[InputRequired()])
+    email = EmailField('Email:', validators=[InputRequired(), Email()])
+    password = PasswordField('Password:', validators=[InputRequired()])
     submit = SubmitField('Login')
+
+
+class CreateAccount(FlaskForm):
+    email = EmailField('Email:', validators=[InputRequired(), Email()])
+    username = StringField('Username:', validators=[InputRequired(), Length(6,16)])
+    password = PasswordField('Password:', validators=[DataRequired(), Length(8, 16)])
+    confirm = PasswordField('Confirm Password:', validators=[DataRequired(), EqualTo('password', message='Passwords must match.')])
+    submit = SubmitField('Create Account')
