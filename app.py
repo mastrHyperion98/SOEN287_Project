0ef4bfd1f092aceaf3c5a99ef1dd74ec167178ff
Steven <s_smith1998@outlook.com>
2020-07-03 01:25:00 -0400

    Cleanup the repository

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ def index():
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
+    print("Updates")
     if session.get('user'):
         next_page = session.get('next', url_for("dashboard"))
         session['next'] = url_for("dashboard")
--- quick_launch.sh
+++ quick_launch.sh
@@ -2,4 +2,5 @@ source venv/bin/activate
 export FLASK_ORIGIN=http://localhost:8080
 export FLASK_APP=app.py
 export FLASK_ENV=development
+export FLASK_DEBUG=1
 flask run
